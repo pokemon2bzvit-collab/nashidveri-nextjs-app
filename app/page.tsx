0332ddcb7013a135151483e3e273007ee2ea344c
pokemon2bzvit-collab <pokemon2bzvit@gmail.com>
2026-08-23 19:52:51 +0300

    головна

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { Contacts } from "@/components/contacts";
 import { Hero } from "@/components/hero";
-import { HomeCategories } from "@/components/home-categories";
 import { SiteShell } from "@/components/site-shell";
-export default function Home() { return <SiteShell><main><Hero onMeasure={() => document.getElementById("contacts")?.scrollIntoView({ behavior: "smooth" })} /><HomeCategories /><Contacts /></main></SiteShell> }
+export default function Home() { return <SiteShell><main><Hero onMeasure={() => window.location.assign("/contacts")} /></main></SiteShell> }
