5769468c29eef765fd901604aecd58cc9a8bbcc0
pokemon2bzvit-collab <pokemon2bzvit@gmail.com>
2026-08-24 20:44:38 +0300

    перший екран

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 import { Hero } from "@/components/hero";
 import { HomeCategories } from "@/components/home-categories";
-import { FinalCta, Process, TrustBar } from "@/components/conversion-sections";
-import { Portfolio } from "@/components/portfolio";
 import { SiteShell } from "@/components/site-shell";
-export default function Home() { return <SiteShell><main><Hero onMeasure={() => window.location.assign("/contacts")} /><TrustBar /><HomeCategories /><Process /><Portfolio /><FinalCta /></main></SiteShell> }
+export default function Home() { return <SiteShell><main><Hero onMeasure={() => window.location.assign("/contacts")} /><HomeCategories /></main></SiteShell> }
