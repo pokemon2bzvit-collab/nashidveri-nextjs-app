c7963fb0ef274ab47b027eb587e315b826a8b2da
pokemon2bzvit-collab <pokemon2bzvit@gmail.com>
2026-09-13 16:51:14 +0300

    лого

--- components/brand-mark.tsx
+++ components/brand-mark.tsx
@@ -1,3 +1,3 @@
 export function BrandMark({ dark: _dark = false }: { dark?: boolean }) {
-  return <img src="/nashi-dveri-logo-v3.svg" alt="Наші двері" className="h-10 w-auto max-w-[156px] object-contain sm:h-11 sm:max-w-[172px]" />;
+  return <img src="/nashi-dveri-logo-v4.svg" alt="Наші двері" className="h-10 w-auto max-w-[156px] object-contain sm:h-11 sm:max-w-[172px]" />;
 }
